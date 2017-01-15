1d8bd03e8642f288fecc6bddd52d2ebb8b9315c6
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-15 18:23:01 +1300

    wiring up graphql service as per feathers tutorial

--- src/services/graphql/index.js
+++ src/services/graphql/index.js
@@ -2,53 +2,36 @@
 
 const hooks = require('./hooks')
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 
 import schema from './schema/'
-
-class Service {
-  constructor (options) {
-    this.options = options || {}
-  }
-
-  find (params) {
-    return Promise.resolve([])
-  }
-
-  get (id, params) {
-    return Promise.resolve({
-      id, text: `A new message with ID: ${id}!`
-    })
-  }
-
-  create (data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)))
-    }
-
-    return Promise.resolve(data)
-  }
-
-  update (id, data, params) {
-    return Promise.resolve(data)
-  }
-
-  patch (id, data, params) {
-    return Promise.resolve(data)
-  }
-
-  remove (id, params) {
-    return Promise.resolve({ id })
-  }
-}
+import resolvers from './resolvers/'
 
 module.exports = function () {
   const app = this
+  const executableSchema = makeExecutableSchema({
+    typeDefs: schema,
+    resolvers: resolvers
+  })
 
   // Initialize our service with any options it requires
-  app.use('/graphql', new Service())
+  app.use('/graphql', graphqlExpress((req) => {
+    let { token, provider } = req.feathers
+    return {
+      schema: executableSchema,
+      context: {
+        token,
+        provider
+      }
+    }
+  }))
+
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+  }))
 
   // Get our initialize service to that we can bind hooks
-  const graphqlService = app.service('/graphqls')
+  const graphqlService = app.service('/graphql')
 
   // Set up our before hooks
   graphqlService.before(hooks.before)
@@ -56,5 +39,3 @@ module.exports = function () {
   // Set up our after hooks
   graphqlService.after(hooks.after)
 }
-
-module.exports.Service = Service
--- src/services/graphql/schema/index.js
+++ src/services/graphql/schema/index.js
@@ -1,41 +1,33 @@
-import { makeExecutableSchema } from 'graphql-tools'
+export default `
+  type Person {
+    id: Int
+    firstName: String
+    lastName: String
+    projects: [Project]
+  }
+  input PersonInput {
+    firstName: String
+    lastName: String
+    projects: [ProjectInput]
+  }
 
-import resolvers from './resolvers'
+  type Project {
+    id: Int
+    title: String
+    people: [Person]
+  }
+  input ProjectInput {
+    title: String
+    people: [PersonInput]
+  }
 
-const schema = `
-type Person {
-  id: Int
-  firstName: String
-  lastName: String
-  projects: [Project]
-}
-input PersonInput {
-  firstName: String
-  lastName: String
-  projects: [ProjectInput]
-}
+  type Query {
+    person(id: Int): Person
+    people: [Person]
+    projects: [Project]
+  }
 
-type Project {
-  id: Int
-  title: String
-  people: [Person]
-}
-input ProjectInput {
-  title: String
-  people: [PersonInput]
-}
-
-type Query {
-  person(id: Int): Person
-  people: [Person]
-  projects: [Project]
-}
-
-type Mutation {
-  addProject(input: ProjectInput): Project
-}
+  type Mutation {
+    addProject(input: ProjectInput): Project
+  }
 `
-export default makeExecutableSchema({
-  typeDefs: schema,
-  resolvers: resolvers
-})
