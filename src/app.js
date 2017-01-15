5e8c9a1c0bbf24754fcbbba646aaa2d93d306ff3
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-15 19:04:29 +1300

    wire up additional config

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import cors from 'cors'
+import compress from 'compression'
 import feathers from 'feathers'
+import hooks from 'feathers-hooks'
 import rest from 'feathers-rest'
 import bodyParser from 'body-parser'
 
@@ -7,7 +10,12 @@ import services from './services'
 const PORT = 3000
 
 const app = feathers()
-app.use(bodyParser.json())
+app.use(compress())
+  .options('*', cors())
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .configure(hooks())
   .configure(rest())
   .configure(services)
 
