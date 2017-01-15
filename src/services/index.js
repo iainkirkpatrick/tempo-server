dbe74c458f1fc6038e522a0b4783be1de1791e0b
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-15 21:59:31 +1300

    fix service export for feathers generator

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,17 @@
 'use strict'
 
+const project = require('./project');
+
 import authentication from './authentication'
 import user from './user'
 import graphql from './graphql'
 
-export default function () {
+// N.B. this needs to be module.exports to work with feathres-generator
+module.exports = function () {
   const app = this
 
   app.configure(authentication)
   app.configure(user)
   app.configure(graphql)
+  app.configure(project);
 }
