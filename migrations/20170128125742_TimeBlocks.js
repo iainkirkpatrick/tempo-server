1e326b6ca99b3a6240463dbe8e7aba7858126ec4
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-28 13:50:55 +1300

    start and end datetimes on TimeBlocks

--- migrations/20170128125742_TimeBlocks.js
+++ migrations/20170128125742_TimeBlocks.js
@@ -2,6 +2,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('TimeBlocks', function (table) {
     table.increments('id')
     table.integer('Developers_ProjectsId').references('Developers_Projects.id')
+    table.dateTime('start')
+    table.dateTime('end')
   })
 }
 
