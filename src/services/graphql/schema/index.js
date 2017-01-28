151bb751e11dd8b85e73466caf1ff2d10a721bf8
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-28 15:30:54 +1300

    update graphql schema

--- src/services/graphql/schema/index.js
+++ src/services/graphql/schema/index.js
@@ -7,21 +7,26 @@ export default `
   }
   input DeveloperInput {
     id: Int
+    firstName: String
+    lastName: String
   }
 
   type Project {
     id: Int
     title: String
+    client: String
     developers: [Developer]
   }
   input ProjectInput {
     title: String
+    client: String
     developers: [DeveloperInput]
   }
 
   type Query {
     developer(id: Int): Developer
     developers: [Developer]
+    project(id: Int): Project
     projects: [Project]
   }
 
