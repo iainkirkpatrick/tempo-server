f0cca7c928da89259feccc488d353c74913351d5
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-28 19:12:36 +1300

    fix seed data, no explicit primary keys, messes up auto-incrementing with future inserts

--- seeds/test-data.js
+++ seeds/test-data.js
@@ -10,24 +10,24 @@ exports.seed = function (knex, Promise) {
     knex('Users').del()
   )
   .then(function () {
-    return knex('Users').insert({ id: 1 })
+    return knex('Users').insert({})
   })
   .then(function () {
-    return knex('Accounts').insert({ id: 1, UserId: 1, password: 'asdfasdf', email: 'test@test.com' })
+    return knex('Accounts').insert({ UserId: 1, password: 'asdfasdf', email: 'test@test.com' })
   })
   .then(function () {
-    return knex('Profiles').insert({ id: 1, UserId: 1, firstName: 'Test', lastName: 'Testerson' })
+    return knex('Profiles').insert({ UserId: 1, firstName: 'Test', lastName: 'Testerson' })
   })
   .then(function () {
-    return knex('Developers').insert({ id: 1, firstName: 'Test', lastName: 'Testerson' })
+    return knex('Developers').insert({ firstName: 'Test', lastName: 'Testerson' })
   })
   .then(function () {
-    return knex('Projects').insert({ id: 1, title: 'A Test Project', client: 'Test Consultants' })
+    return knex('Projects').insert({ title: 'A Test Project', client: 'Test Consultants' })
   })
   .then(function () {
-    return knex('Developers_Projects').insert({ id: 1, DeveloperId: 1, ProjectId: 1 })
+    return knex('Developers_Projects').insert({ DeveloperId: 1, ProjectId: 1 })
   })
   .then(function () {
-    return knex('TimeBlocks').insert({ id: 1, Developers_ProjectsId: 1, start: new Date('2017-01-23'), end: new Date('2017-01-27') })
+    return knex('TimeBlocks').insert({ Developers_ProjectsId: 1, start: new Date('2017-01-23'), end: new Date('2017-01-27') })
   })
 }
